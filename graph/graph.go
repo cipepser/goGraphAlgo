7963aaa586aba5b677daef15f9ad2d23100acc55
cipepser <respepic@gmail.com>
2018-01-26 15:31:15 +0900

    implement AddVertex

--- graph/graph.go
+++ graph/graph.go
@@ -19,13 +19,15 @@ type graph struct {
 
 // NewGraph constructs a new graph
 func NewGraph() *graph {
-	return new(graph)
+	g := new(graph)
+	g.vertices = map[Vertex]int{}
+	return g
 }
 
 // AddVertex adds a vertex v to the graph g.
 // If a graph has already have the vertex v, it returns an error
 func (g *graph) AddVertex(v Vertex) error {
-	if g.vertices[v] > 0 {
+	if _, ok := g.vertices[v]; ok {
 		return errors.New("g has already have the vertex v")
 	}
 	g.vertices[v]++
--- graph/graph_test.go
+++ graph/graph_test.go
@@ -12,3 +12,17 @@ func TestIsDirected(t *testing.T) {
 		t.Errorf("\nexpected: %v\nactual: %v", expected, actual)
 	}
 }
+
+func TestAddVertex(t *testing.T) {
+	g := NewGraph()
+
+	// first time to add a vertex 0
+	if err := g.AddVertex(0); err != nil {
+		t.Error()
+	}
+
+	// sencond time to add a vertex 0, expects returnning error
+	if err := g.AddVertex(0); err == nil {
+		t.Error()
+	}
+}
