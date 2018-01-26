7f9727da2cea7632cabfc3afc3bc666d2b79fd02
cipepser <respepic@gmail.com>
2018-01-26 14:49:53 +0900

    draft of the functions of add/remove node and edge

--- graph/graph.go
+++ graph/graph.go
@@ -8,10 +8,32 @@ type Edge struct {
 }
 
 type graph struct {
+	vertices   []Vertex
+	edges      []Edge
 	isDirected bool
 }
 
 // NewGraph constructs a new graph
 func NewGraph() *graph {
 	return new(graph)
+}
+
+func (g *graph) AddNode() error {
+
+	return nil
+}
+
+func (g *graph) RemoveNode() error {
+
+	return nil
+}
+
+func (g *graph) AddEdge() error {
+
+	return nil
+}
+
+func (g *graph) RemoveEdge() error {
+
+	return nil
 }
