90abfc889e9da526c892077afde193c36d73e819
cipepser <respepic@gmail.com>
2018-01-27 01:01:11 +0900

    use ExistsVertex in AddVertex and RemoveVertex

--- graph/graph.go
+++ graph/graph.go
@@ -51,7 +51,7 @@ func (g *graph) ExistsVertex(v Vertex) bool {
 // AddVertex adds a vertex v to the graph g.
 // If a graph has already have the vertex v, it returns an error
 func (g *graph) AddVertex(v Vertex) error {
-	if _, ok := g.vertices[v]; ok {
+	if g.ExistsVertex(v) {
 		return errors.New("g has already have the vertex v")
 	}
 	g.vertices[v]++
@@ -61,7 +61,7 @@ func (g *graph) AddVertex(v Vertex) error {
 
 // RemoveVertex remove a vertex form the graph g.
 func (g *graph) RemoveVertex(v Vertex) error {
-	if _, ok := g.vertices[v]; !ok {
+	if !g.ExistsVertex(v) {
 		return errors.New("input v doesn't exist in g")
 	}
 
